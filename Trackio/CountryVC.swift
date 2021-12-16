920030b78537bcff1370f90939f257eb0da075ef
harungunes <h.gunes13@gmail.com>
2021-12-17 00:39:55 +0100

    create CountryVC

---
+++ Trackio/CountryVC.swift
@@ -0,0 +1,16 @@
+//
+//  CountryVC.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 17/12/2021.
+//
+
+import UIKit
+
+class CountryVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemRed
+  }
+}
