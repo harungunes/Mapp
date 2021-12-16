4e7a5a63270e5dc755d59ae160b42937d465df8a
harungunes <h.gunes13@gmail.com>
2021-12-16 23:03:45 +0100

    create CountryListVC

---
+++ Trackio/CountryListVC.swift
@@ -0,0 +1,16 @@
+//
+//  CountryListVC.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 16/12/2021.
+//
+
+import UIKit
+
+class CountryListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemGreen
+  }
+}
