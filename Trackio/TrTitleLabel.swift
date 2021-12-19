0090b4570dbfd200e5979b20a782adf6a7f23b32
harungunes <h.gunes13@gmail.com>
2021-12-19 21:33:19 +0100

    create reusable title label TrTitleLabel

---
+++ Trackio/TrTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  TrTitleLabel.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 19/12/2021.
+//
+
+import UIKit
+
+class TrTitleLabel: UILabel {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+    super.init(frame: .zero)
+    self.textAlignment = textAlignment
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    lineBreakMode = .byTruncatingTail
+    textColor = .label
+    minimumScaleFactor = 0.9
+  }
+}
