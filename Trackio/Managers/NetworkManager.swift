b21ede3a99aded04d3e60f2e7ad45d35cc43793f
harungunes <h.gunes13@gmail.com>
2021-12-22 21:31:19 +0100

    create network manager

---
+++ Trackio/Managers/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 22/12/2021.
+//
+
+import Foundation
+
+class NetworkManager {
+  static let shared = NetworkManager()
+  
+  private init() {}
+  
+  
+}
