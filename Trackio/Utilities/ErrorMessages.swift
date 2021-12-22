b6e44610b3ed64af658fb39e43f5df01dadbbce6
harungunes <h.gunes13@gmail.com>
2021-12-22 23:23:02 +0100

    create ErrorMessage enum

---
+++ Trackio/Utilities/ErrorMessages.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessages.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 22/12/2021.
+//
+
+import Foundation
+
+enum ErrorMessages: String {
+  case invalidUsername = "This username created an invalid request. Please try again"
+  case unableToComplete = "Unable to complete your request. Please check your internet connection."
+  case invalidResponse = "Invalid response from the server. Please try again."
+  case invalidData = "The data received from the server was invalid. Please try again"
+}
