cce3db78279d52da8177574a5918f9deaeb37fef
harungunes <h.gunes13@gmail.com>
2022-01-15 17:45:34 +0100

    call the network data in CountryListVC

--- Trackio/Controllers/CountryListVC.swift
+++ Trackio/Controllers/CountryListVC.swift
@@ -9,15 +9,31 @@ import UIKit
 
 class CountryListVC: UIViewController {
   
+  var countryList: [Country] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     title = "Trackio"
     configureNavigationBar()
+    getCountryData()
   }
   
   private func configureNavigationBar() {
     navigationController?.isNavigationBarHidden = false
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  func getCountryData() {
+    NetworkManager.shared.getCountryData { result in
+      switch result {
+      case .failure(let error):
+        self.presentTrAlertVC(title: "Bad stuff happened here", body: error.rawValue, buttonTitle: "Ok")
+      case .success(let countries):
+        self.countryList = countries
+        
+        print(self.countryList[0])
+      }
+    }
   }
 }
