e7ba77b990ab2833c6eb9d2129285290355d3787
harungunes <h.gunes13@gmail.com>
2022-01-22 17:47:25 +0100

    create CountryInfoHeaderVC as child vc

---
+++ Trackio/Views/ViewControllers/CountryInfoHeaderVC.swift
@@ -0,0 +1,17 @@
+//
+//  CountryInfoHeaderVC.swift
+//  Trackio
+//
+//  Created by Harun Gunes on 22/01/2022.
+//
+
+import UIKit
+
+class CountryInfoHeaderVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
