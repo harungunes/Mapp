12a804e922d9a9210303bb372f751fab8df6a31f
harungunes <h.gunes13@gmail.com>
2022-02-12 20:51:11 +0100

    initialize TrItemInfoView

--- Trackio/Views/CustomViews/TrItemInfoView.swift
+++ Trackio/Views/CustomViews/TrItemInfoView.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class TrItemInfoView: UIView {
   
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemPurple
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
